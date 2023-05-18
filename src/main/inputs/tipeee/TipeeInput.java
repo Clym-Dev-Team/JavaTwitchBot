0a6f423faeaafde81409edfb487a583367ba6ecc
orciument <orciument@gmail.com>
2023-05-18 02:04:41 +0200

    - Added @Input and @Component Annotation to make the Input Accessibly to Bot Core - Added more Logging and Debug output - Added @Value Annotations so that the APIKey and Channel name are automatically grabbed from the Env. Vars

--- src/main/inputs/tipeee/TipeeInput.java
+++ src/main/inputs/tipeee/TipeeInput.java
@@ -2,18 +2,35 @@ package main.inputs.tipeee;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
-import main.system.inputSystem.TipeeStreamInput;
+import main.system.inputSystem.Input;
+import main.system.inputSystem.TwitchBotInput;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.net.URISyntaxException;
+import java.time.Instant;
+import java.util.Arrays;
 
-public class TipeeInput implements TipeeStreamInput {
+@Input
+@Component
+public class TipeeInput implements TwitchBotInput {
 
-    private String apiKey;
-    private String channelName;
+    @Value("${tipeee_apiKey}")
+    public void setApiKey(String apiKey) {
+        TipeeInput.apiKey = apiKey;
+    }
+    private static String apiKey;
+
+    @Value("${tipeee_channel}")
+    public void setChannelName(String channelName) {
+        TipeeInput.channelName = channelName;
+    }
+    private static String channelName;
+
     private Socket socket;
     private static final Logger LOGGER = LoggerFactory.getLogger(TipeeInput.class);
 
@@ -24,22 +41,23 @@ public class TipeeInput implements TipeeStreamInput {
 
     @Override
     public boolean shutdown() {
-        if (socket.isActive()) {
-            socket.close();
-            LOGGER.info("Shut down Tipeee input");
-        }
+        socket.close();
+        LOGGER.info("Shut down Tipeee input");
 
         return true;
     }
 
     @Override
     public void run() {
+        LOGGER.info("Stating TipeeeInput for Channel " + channelName);
         try {
             socket = IO.socket("https://sso-cf.tipeeestream.com:443?access_token=" + apiKey);
-            socket.on("new-event", objects -> {
 
+            socket.on("new-event", data -> {
+                TipeeeEventHandler.handleDonationEvent(Arrays.toString(data));
             });
-            socket.on("connect", objects -> {
+
+            socket.on(Socket.EVENT_CONNECT, objects -> {
                 try {
                     socket.emit("join-room", new JSONObject()
                             .put("room", apiKey)
@@ -48,10 +66,17 @@ public class TipeeInput implements TipeeStreamInput {
                     throw new RuntimeException(e);
                 }
             });
+
             socket.connect();
+            //Wait for the Socket to connect, because it will be opened in a new Thread and
+            // so will otherwise not be done by the time we check if the starting has worked
+            Instant end = Instant.now().plusSeconds(10);
+            while (!socket.connected() || end.isBefore(Instant.now()))
+                Thread.onSpinWait();
+
             LOGGER.info("Tipeee socket connected");
         } catch (URISyntaxException e) {
-            LOGGER.error("Could not conntect Tipeee socket");
+            LOGGER.error("Could not connect to Tipeee socket");
             throw new RuntimeException(e);
         }
     }
@@ -59,9 +84,5 @@ public class TipeeInput implements TipeeStreamInput {
     @Override
     public boolean running() {
         return socket.connected();
-    }
-
-    private void callEvent() {
-
     }
 }
