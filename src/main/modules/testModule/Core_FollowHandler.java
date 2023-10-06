ec6002958d2b0aaef7ad4b97259cd2136ca04524
Orciument <orciument@gmail.com>
2023-10-06 17:28:16 +0200

    - removed requirement for Subscriber to state type - renamed Subscribe to Subscriber - refactored EventDispatcher

--- src/main/modules/testModule/Core_FollowHandler.java
+++ src/main/modules/testModule/Core_FollowHandler.java
@@ -2,17 +2,17 @@ package main.modules.testModule;
 
 import com.github.twitch4j.chat.events.channel.FollowEvent;
 import com.github.twitch4j.chat.events.channel.SubscriptionEvent;
-import main.system.eventSystem.Subscribe;
+import main.system.eventSystem.Subscriber;
 
 @SuppressWarnings("unused")
 public class Core_FollowHandler {
 
-    @Subscribe(EventClass = FollowEvent.class)
+    @Subscriber
     public static void handleFollow(FollowEvent followEvent) {
         System.out.println("Test follow Event");
     }
 
-    @Subscribe(EventClass = SubscriptionEvent.class)
+    @Subscriber
     public static void handleSub(SubscriptionEvent subscriptionEvent) {
         System.out.println("Sub follow Event");
     }
