ddc8400a71275ba7eb824ffdf0206eefe467de84
orciument <orciument@gmail.com>
2024-01-28 00:15:58 +0100

    Added a OAuth Request

---
+++ src/main/inputs/shared/oauth/OauthAccount.java
@@ -0,0 +1,34 @@
+package main.inputs.shared.oauth;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "oauth_accounts")
+@Component
+public class OauthAccount {
+    @Id
+    public String accName;
+    public String service;
+    public String refreshToken;
+
+    public OauthAccount() {
+    }
+
+    public OauthAccount(String accName, String service, String refreshToken) {
+        this.accName = accName;
+        this.service = service;
+        this.refreshToken = refreshToken;
+    }
+
+    public static OauthAccountRepo repo;
+
+    @Autowired
+    public void setOauthAccountRepo(OauthAccountRepo oauthAccountRepo) {
+        repo = oauthAccountRepo;
+    }
+
+}
+
+
