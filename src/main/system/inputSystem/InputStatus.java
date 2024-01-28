281edb92b0317c922e4c28ed9b9120b5d6de3506
orciument <orciument@gmail.com>
2024-01-28 18:57:26 +0100

    Replace running() with Health System

---
+++ src/main/system/inputSystem/InputStatus.java
@@ -0,0 +1,23 @@
+package main.system.inputSystem;
+
+/**
+ * Used to Indicates the state of an Input <br/>
+ * - injured is for a partial failure <br/>
+ * - dead is for a full or almost complete failure/crash <br/>
+ * - disabled should not be set by an input inself, it is to indicate that an Input was found, but will not be started <br/>
+ */
+public enum InputStatus {
+    disabled,
+    healthy,
+    starting,
+    injured,
+    dead;
+
+    public boolean isWorseThan(InputStatus status) {
+        return (this.compareTo(status) > 0);
+    }
+
+    public boolean isBetterThan(InputStatus status) {
+        return (this.compareTo(status) < 0);
+    }
+}
