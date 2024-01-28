8bc50869f5c1798e8f61ec07999911840e0e2a16
orciument <orciument@gmail.com>
2024-01-28 23:52:15 +0100

    Rework InputManager, move statereporting into HealthManager

--- src/main/system/inputSystem/HealthManager.java
+++ src/main/system/inputSystem/HealthManager.java
@@ -1,6 +1,10 @@
 package main.system.inputSystem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class HealthManager {
 
@@ -14,8 +18,26 @@ public class HealthManager {
         }
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(HealthManager.class);
     private static final ArrayList<Status> statuses = new ArrayList<>();
-    private static InputStatus overallStatus = InputStatus.disabled;
+    private static volatile InputStatus overallStatus = InputStatus.STOPPED;
+    private static volatile Consumer<InputStatus> callback;
+    private static volatile InputStatus filter;
+
+    public static void subscribeNextChange(Consumer<InputStatus> callback, InputStatus filterBy) {
+        HealthManager.callback = callback;
+        filter = filterBy;
+    }
+
+    private static void checkCallback() {
+        if (filter != overallStatus) {
+            return;
+        }
+        if (callback != null) {
+            callback.accept(overallStatus);
+        }
+        callback = null;
+    }
 
     public static void reportStatus(BotInput input, InputStatus status) {
         var statusOptional = statuses.stream().filter(s -> s.input == input).findFirst();
@@ -25,19 +47,20 @@ public class HealthManager {
             statusOptional.get().status = status;
         }
         checkOverallStatusChange();
+        checkCallback();
     }
 
     private static void checkOverallStatusChange() {
         var worstFound = calcOverallStatus();
         if (overallStatus != worstFound) {
             overallStatus = worstFound;
-            System.out.println("New Overall status is: " + overallStatus);
+            logger.info("INPUTS: " + overallStatus);
         }
     }
 
     private static InputStatus calcOverallStatus() {
-        InputStatus worstYet = InputStatus.disabled;
-        for (Status status: statuses) {
+        InputStatus worstYet = InputStatus.STOPPED;
+        for (Status status : statuses) {
             if (status.status.compareTo(worstYet) > 0) {
                 worstYet = status.status;
             }
