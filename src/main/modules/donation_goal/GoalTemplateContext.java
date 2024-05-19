e668fdce18b70936e8114e2393fe6247afdb71fc
orciument <orciument@gmail.com>
2024-05-19 02:41:17 +0200

    added formating for Goal template context, and added donation template context

--- src/main/modules/donation_goal/GoalTemplateContext.java
+++ src/main/modules/donation_goal/GoalTemplateContext.java
@@ -1,9 +1,23 @@
 package main.modules.donation_goal;
 
-public record GoalTemplateContext(
-        String targetAmount,
-        String currentAmount,
-        String currentPercent,
-        String displayName,
-        String progressBar
-) {}
+import main.system.ASCIIProgressbar;
+
+public class GoalTemplateContext {
+    public final String targetAmount;
+    public final String currentAmount;
+    public final String currentPercentDecimals;
+    public final String currentPercentRounded;
+    public final String displayName;
+    public final String progressBar;
+
+
+    public GoalTemplateContext(DonationGoal goal) {
+        this.targetAmount = String.format("%.2f", goal.targetAmount);
+        this.currentAmount = String.format("%.2f", goal.amountInGoal);
+        double percent = (goal.amountInGoal / goal.targetAmount) * 100;
+        this.currentPercentDecimals = String.format("%.2f", percent);
+        this.currentPercentRounded = String.format("%.0f", percent);
+        this.displayName = goal.displayName;
+        this.progressBar = ASCIIProgressbar.bar(goal.amountInGoal, goal.targetAmount, 10, "■", " ", false, false);
+    }
+}
