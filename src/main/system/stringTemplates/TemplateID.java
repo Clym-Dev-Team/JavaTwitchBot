234e72d65aebad6c96965df6ee9e1f12cd04a842
orciument <orciument@gmail.com>
2024-05-19 02:41:57 +0200

    make out public, add objectName field to template ID

--- src/main/system/stringTemplates/TemplateID.java
+++ src/main/system/stringTemplates/TemplateID.java
@@ -5,12 +5,14 @@ import java.io.Serializable;
 public class TemplateID implements Serializable {
     private String type;
     private String name;
+    private String objectName;
 
     public TemplateID() {
     }
 
-    public TemplateID(String type, String name) {
+    public TemplateID(String type, String name, String objectName) {
         this.type = type;
         this.name = name;
+        this.objectName = objectName;
     }
 }
