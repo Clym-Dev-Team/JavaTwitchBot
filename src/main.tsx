cd2af7bc7b1b5f88ff7bcec511508597cc67cf1b
orciument <orciument@gmail.com>
2024-05-27 00:59:51 +0200

    upgrade to v6 BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,28 +4,51 @@ import './index.css'
 import './main.css'
 import {GoalEditorPane} from "./components/Goals/GoalEditor.tsx";
 import Test from "./components/Test.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromChildren, Outlet, Route, RouterProvider} from "react-router-dom";
 import MessagePane from "./components/ChatHistory/Pane/MessagePane.tsx";
 import TemplateEditorPane from "./components/Templates/TemplateEditor/TemplateEditorPane.tsx";
 import TemplateListPane from "./components/Templates/TemplateList/TemplateListPane.tsx";
 import NavMenu from "./components/NavMenu/NavMenu.tsx";
 import AuthProvider from "./components/Login/AuthProvider.tsx";
 
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <Route element={<Layout/>}>
+      <Route path="/goal" element={<GoalEditorPane/>}/>
+      <Route path="/templates" element={<TemplateListPane/>}/>
+      <Route path="/templates/edit" element={<TemplateEditorPane/>}/>
+      <Route path="/test" element={<Test/>}/>
+      <Route path="/history" element={<MessagePane/>}/>
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-          <NavMenu/>
-          <div className="contentRoot">
-            <Routes>
-              <Route path="/goal" element={<GoalEditorPane/>}/>
-              <Route path="/templates" element={<TemplateListPane/>}/>
-              <Route path="/templates/edit" element={<TemplateEditorPane/>}/>
-              <Route path="/test" element={<Test/>}/>
-              <Route path="/history" element={<MessagePane/>}/>
-            </Routes>
-          </div>
-      </BrowserRouter>
+      <RouterProvider router={router}>
+      </RouterProvider>
+      {/*<BrowserRouter>*/}
+      {/*  <NavMenu/>*/}
+      {/*  <div className="contentRoot">*/}
+      {/*    <Routes>*/}
+      {/*      <Route path="/goal" element={<GoalEditorPane/>}/>*/}
+      {/*      <Route path="/templates" element={<TemplateListPane/>}/>*/}
+      {/*      <Route path="/templates/edit" element={<TemplateEditorPane/>}/>*/}
+      {/*      <Route path="/test" element={<Test/>}/>*/}
+      {/*      <Route path="/history" element={<MessagePane/>}/>*/}
+      {/*    </Routes>*/}
+      {/*  </div>*/}
+      {/*</BrowserRouter>*/}
     </AuthProvider>
   </React.StrictMode>,
 )
+
+function Layout() {
+  return <>
+    <NavMenu/>
+    <div className="contentRoot">
+      <Outlet/>
+    </div>
+  </>;
+}
