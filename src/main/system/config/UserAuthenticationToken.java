55681c8b5192e453a19c098e90720cdf51127fad
orciument <orciument@gmail.com>
2024-05-28 03:08:44 +0200

    fix infinite loop because successfulAuthentication wasn't handled

---
+++ src/main/system/config/UserAuthenticationToken.java
@@ -0,0 +1,65 @@
+package main.system.config;
+
+import main.system.panelAuth.BotUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.jackson2.SimpleGrantedAuthorityMixin;
+
+import java.util.*;
+
+public class UserAuthenticationToken implements Authentication {
+    String accessToken;
+    boolean isAuthenticated;
+    Optional<BotUser> botUser;
+    List<SimpleGrantedAuthority> grantedAuthorities;
+
+    public UserAuthenticationToken(String accessToken, boolean isAuthenticated, BotUser botUser, SimpleGrantedAuthority ...authority) {
+        this.accessToken = accessToken;
+        this.isAuthenticated = isAuthenticated;
+        this.botUser = Optional.ofNullable(botUser);
+        this.grantedAuthorities = Arrays.stream(authority).toList();
+    }
+
+    public UserAuthenticationToken(String accessToken, boolean isAuthenticated) {
+        this.accessToken = accessToken;
+        this.isAuthenticated = isAuthenticated;
+        this.botUser = Optional.empty();
+        this.grantedAuthorities = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return botUser.orElse(null);
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return accessToken;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        this.isAuthenticated = isAuthenticated;
+    }
+
+    @Override
+    public String getName() {
+        return botUser.isEmpty() ? accessToken : botUser.get().username();
+    }
+}
