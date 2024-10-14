4028c834d5332097bbe4d66250a2970e8dc97ec2
orciument <orciument@gmail.com>
2024-10-15 01:55:18 +0200

    add new tests for TemplateLexer

--- src/test/talium/system/templateParser/TemplateLexerTest.java
+++ src/test/talium/system/templateParser/TemplateLexerTest.java
@@ -1,10 +1,38 @@
 package talium.system.templateParser;
 
+import org.junit.jupiter.api.Test;
+import talium.system.templateParser.majorParser.TemplateLexer;
+
+// correct behaviour for now is to just fail semi silently and try to output as much as possible into the chat
+// this tests if any of these parsing attempts throw an exception
 public class TemplateLexerTest {
-    //TODO inject string, walk entire string and test if all chars are given out
-    //TODO test if eof is done correctly at different parts of a string
-    //TODO check peek does not advance
-    //TODO check that next advances
-    //TODO check malformed template, with missing }
-    //TODO check missing $ or %
+    @Test
+    void missing_closing_bracket() {
+        TemplateLexer lex = new TemplateLexer("Hello, ${var.name!");
+        System.out.println(lex.parse());
+    }
+
+    @Test
+    void missing_dollar() {
+        TemplateLexer lex = new TemplateLexer("Hello, {var.name}!");
+        System.out.println(lex.parse());
+    }
+
+    @Test
+    void missing_closing_bracket_directive() {
+        TemplateLexer lex = new TemplateLexer("Hello, %{ if var.name != \"\" }${var.name}%{ else }unnamed%{ endif ");
+        System.out.println(lex.parse());
+    }
+
+    @Test
+    void missing_percent() {
+        TemplateLexer lex = new TemplateLexer("Hello, %{ if var.name != \"\" }${var.name}%{ else }unnamed{ endif }");
+        System.out.println(lex.parse());
+    }
+
+    @Test
+    void malformed_endIf() {
+        TemplateLexer lex = new TemplateLexer("Hello, %{ if var.name != \"\" }${var.name}%{ else }unnamed%{ endif dadasdadad }");
+        System.out.println(lex.parse());
+    }
 }
