d902ea7fdba2a1c29fbe0a48fcf685af77190658
orciument <orciument@gmail.com>
2024-10-17 14:55:23 +0200

    fix eventDispatcher package scan, improved logging in Twitch4JInput

--- src/main/talium/system/eventSystem/Subscriber.java
+++ src/main/talium/system/eventSystem/Subscriber.java
@@ -5,13 +5,12 @@ import java.lang.annotation.*;
 /**
  * This Annotation is for Subscribing to an Object on the EventStream.
  * A Subscriber can be created by creating by Annotating a Method with this Annotation, and specifying
- * the Class it subscribes to via this Annotations EventClass Parameter. <br/>
+ * the Class it subscribes to via its Parameter. <br/>
  * <br/>
  * For a Subscriber to be valid it needs to be: <br/>
- * - located in a subfolder of main.modules
+ * - located in a subfolder of talium.modules
  * - static <br/>
  * - only have one Parameter <br/>
- * - the Parameter needs to be of the same Class as specified in the Annotation <br/>
  *
  * @see EventDispatcher
  */
