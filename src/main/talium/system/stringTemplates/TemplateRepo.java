3de339e76c5eea03b8edde9ff97a51d732f5f010
orciument <orciument@gmail.com>
2024-10-21 11:28:31 +0200

    combine template id into single string

--- src/main/talium/system/stringTemplates/TemplateRepo.java
+++ src/main/talium/system/stringTemplates/TemplateRepo.java
@@ -1,6 +1,5 @@
 package talium.system.stringTemplates;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,12 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface TemplateRepo extends CrudRepository<Template, TemplateID> {
+public interface TemplateRepo extends CrudRepository<Template, String> {
 
-    Optional<Template> findByModuleAndTypeAndObject(String module, String type, String object);
-
-    @Transactional
-    @Modifying
-    @Query("update Template t set t.template = ?1 where t.module = ?2 and t.type = ?3 and t.object = ?4")
-    void updateTemplateByTypeAndNameAndObjectName(String template, String module, String type, String object);
 }
