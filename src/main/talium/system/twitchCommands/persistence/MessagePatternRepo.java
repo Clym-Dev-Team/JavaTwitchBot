ec8598a6c827e048e35172a649e16c30c0c4d7eb
orciument <orciument@gmail.com>
2024-10-24 23:17:29 +0200

    rename chatTrigger dir to twitchCommands

--- src/main/talium/system/twitchCommands/persistence/MessagePatternRepo.java
+++ src/main/talium/system/twitchCommands/persistence/MessagePatternRepo.java
@@ -1,4 +1,4 @@
-package talium.system.chatTrigger.persistence;
+package talium.system.twitchCommands.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
