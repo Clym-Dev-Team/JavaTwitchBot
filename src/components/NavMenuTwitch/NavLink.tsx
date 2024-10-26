b9ba78157fbf2f0cdb9a175b9c842267037336bc
orciument <orciument@gmail.com>
2024-10-26 18:28:57 +0200

    a bit of docs

--- src/components/NavMenuTwitch/NavLink.tsx
+++ src/components/NavMenuTwitch/NavLink.tsx
@@ -2,12 +2,26 @@ import {Link, useLocation} from "react-router-dom";
 import React, {useEffect} from "react";
 import "./NavLink.css"
 
+/**
+ * @param target the url to point to, and to highlight when on
+ * @param allowChildren if child paths are also considered matching, true by default
+ * @param name display name/text of the link
+ */
 export interface NavLinkProps {
   target: string,
   allowChildren?: boolean,
   name: string
 }
 
+/**
+ * Displays a Link with the given name and url in the style of our navigation menu. Is Highlighted when the page Url matches the target url, or a sub path of the url. </br>
+ * E.g.: target: '/test' would match url: '/test/child'
+ *
+ * @param target the url to point to, and to highlight when on
+ * @param allowChildren if child paths are also considered matching, true by default
+ * @param name display name/text of the link
+ * @constructor
+ */
 export default function NavLink({target, allowChildren, name}: NavLinkProps) {
   const {pathname} = useLocation();
   const [highlight, setHighlight] = React.useState(false);
@@ -18,7 +32,7 @@ export default function NavLink({target, allowChildren, name}: NavLinkProps) {
     } else {
       setHighlight(pathname == target);
     }
-  }, [pathname, target]);
+  }, [allowChildren, pathname, target]);
 
   return <Link to={target} className={"navLink " + (highlight ? "navLinkHighlight" : "")}>{name}</Link>
 }
