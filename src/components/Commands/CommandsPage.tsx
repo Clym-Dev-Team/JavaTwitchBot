250876d72ffcaacd39f9323f40b965f34e68729b
orciument <orciument@gmail.com>
2024-10-26 18:50:15 +0200

    add commandPage tabs

---
+++ src/components/Commands/CommandsPage.tsx
@@ -0,0 +1,23 @@
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@shadcn/components/ui/tabs.tsx";
+import CommandList from "./CommandList.tsx";
+import TemplateListPane from "../Templates/TemplateList/TemplateListPane.tsx";
+import "./CommandPage.css"
+
+export default function CommandsPage() {
+  return <Tabs defaultValue="commands" >
+    <TabsList className="commandTabList">
+      <TabsTrigger value="commands">Commands</TabsTrigger>
+      <TabsTrigger value="templates">Templates</TabsTrigger>
+      <TabsTrigger value="trigger">Trigger</TabsTrigger>
+    </TabsList>
+    <TabsContent value="commands">
+      <CommandList/>
+    </TabsContent>
+    <TabsContent value="templates">
+      <TemplateListPane/>
+    </TabsContent>
+    <TabsContent value="trigger">
+      <p>Trigger PlaceHolder</p>
+    </TabsContent>
+  </Tabs>
+}
