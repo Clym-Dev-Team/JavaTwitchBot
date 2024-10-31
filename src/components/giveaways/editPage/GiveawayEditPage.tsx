56ad12c6358035b5201d51bf3a93c0c79ebc4dcd
orciument <orciument@gmail.com>
2024-10-31 11:56:09 +0100

    add 3 columns and flex: 1

--- src/components/giveaways/editPage/GiveawayEditPage.tsx
+++ src/components/giveaways/editPage/GiveawayEditPage.tsx
@@ -1,6 +1,10 @@
 import "./GiveawayEditPage.css"
 import Placeholder from "../../Placeholder/Placeholder.tsx";
 import {Button} from "@shadcn/components/ui/button.tsx";
+import {Accordion, AccordionItem, AccordionTrigger} from "@shadcn/components/ui/accordion.tsx";
+import {AccordionContent} from "@radix-ui/react-accordion";
+import {ScrollArea} from "../../ui/scroll-area.tsx";
+import {Label} from "@shadcn/components/ui/label.tsx";
 
 export interface GiveawayEditViewProps {
 
@@ -10,11 +14,34 @@ export default function GiveawayEditPage(props: GiveawayEditViewProps) {
   return <div className="giveawayEditPage">
     <div className="tileBar">
       <Button variant="default">Save</Button>
-      Edit: !testGW</div>
-    <div className="contentBorder">
-      <div className="formContent">
-        <Placeholder/>
-      </div>
+      Edit: !testGW
     </div>
+    <ScrollArea className="contentBorder">
+        <div className="formContent">
+          <div className="column">
+            <h1>Primary Fields</h1>
+            <div>
+              <Label>
+
+              </Label>
+            </div>
+          </div>
+          <div className="column">
+            <h1>Reminder Timer</h1>
+            <div></div>
+            <h1>Public Website</h1>
+            <div></div>
+          </div>
+          <div className="column">
+            <div className="winnersList">
+              <h1>Winner List</h1>
+            </div>
+            <div className="logs">
+              <h1>Logs</h1>
+            </div>
+          </div>
+          {/*<Placeholder/>*/}
+        </div>
+    </ScrollArea>
   </div>
 }
