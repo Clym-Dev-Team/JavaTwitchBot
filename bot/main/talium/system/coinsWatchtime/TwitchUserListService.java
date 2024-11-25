8e846871f3de3617eca1b2d2e9b17c3e50458d13
orciument <orciument@gmail.com>
2024-11-26 00:20:39 +0100

    start coins inline

--- bot/main/talium/system/coinsWatchtime/TwitchUserListService.java
+++ bot/main/talium/system/coinsWatchtime/TwitchUserListService.java
@@ -4,34 +4,41 @@ import com.github.twitch4j.helix.domain.Chatter;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import talium.inputs.Twitch4J.TwitchApi;
+import talium.system.coinsWatchtime.chatter.ChatterService;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
-import static talium.system.coinsWatchtime.CoinsService.addCoins;
-import static talium.system.coinsWatchtime.WatchtimeService.addMinuteOfWatchtime;
-
 /**
  * Requests the twitch user list (list of active chatters) and computes the difference between the two sets.
  * These differences are then used by coins and watchtime services to add coins and watchtime for each user
  */
+@Component
 public class TwitchUserListService {
-    private static List<String> viewerList = new ArrayList<>();
     private static final Logger logger = LoggerFactory.getLogger(TwitchUserListService.class);
 
     private static final ScheduledExecutorService CHATTER_UPDATE_SERVICE;
 
+    private static ChatterService chatterService;
+    private static final int POLLING_INTERVAL_SECONDS = 60;
+    private static final int COIN_PAYOUT_INTERVAL_SECONDS = 600;
+    private static final int COIN_PAYOUT_AMOUNT = 1;
+
+    @Autowired
+    public void setChatterService(ChatterService chatterService) {
+        TwitchUserListService.chatterService = chatterService;
+    }
+
     static {
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("CHATTER_UPDATE_EXECUTOR").build();
         CHATTER_UPDATE_SERVICE = Executors.newSingleThreadScheduledExecutor(namedThreadFactory);
-        CHATTER_UPDATE_SERVICE.scheduleAtFixedRate(TwitchUserListService::refreshUserList, 0, 1, TimeUnit.MINUTES);
+        CHATTER_UPDATE_SERVICE.scheduleAtFixedRate(TwitchUserListService::refreshUserList, 0, POLLING_INTERVAL_SECONDS, TimeUnit.SECONDS);
     }
 
     private static List<String> getUserList() {
@@ -42,28 +49,16 @@ public class TwitchUserListService {
         // catch because otherwise the scheduler would end on any runtime error
         try {
             logger.info("Refreshing user list");
-            var oldUserList = viewerList;
-            viewerList = getUserList();
-            var leftChatters = getLeftChatters(oldUserList, viewerList);
-//            var isOnline = TwitchApi.isOnline();
-            var isOnline = true;
-            logger.debug("Channel online: {}", isOnline);
-            addMinuteOfWatchtime(viewerList, isOnline);
-            addCoins(viewerList, leftChatters.stream().toList(), isOnline);
+            if (!TwitchApi.isOnline()) {
+                logger.debug("Channel online: false");
+                return;
+            }
+            logger.debug("Channel online: true");
+            var viewerList = getUserList();
+            chatterService.addWatchtimeSeconds(viewerList, 60);
+            chatterService.addCoinsToUser(viewerList, POLLING_INTERVAL_SECONDS, COIN_PAYOUT_INTERVAL_SECONDS, COIN_PAYOUT_AMOUNT);
         } catch (Exception e) {
             logger.error("Failed to refresh user list", e);
         }
-    }
-
-    private static Set<String> getLeftChatters(List<String> lastChatters, List<String> currentChatters) {
-        var setCopy = new HashSet<>(lastChatters);
-        currentChatters.forEach(setCopy::remove);
-        return setCopy;
-    }
-
-    private static Set<String> getJoinedChatters(List<String> lastChatters, List<String> currentChatters) {
-        var setCopy = new HashSet<>(currentChatters);
-        lastChatters.forEach(setCopy::remove);
-        return setCopy;
     }
 }
