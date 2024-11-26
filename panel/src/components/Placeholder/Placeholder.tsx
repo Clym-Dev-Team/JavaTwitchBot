a9bf30e90df745632d4b2146491fcdaeb3f18aaf
orciument <orciument@gmail.com>
2024-11-26 18:17:58 +0100

    improve placeholder

--- panel/src/components/Placeholder/Placeholder.tsx
+++ panel/src/components/Placeholder/Placeholder.tsx
@@ -1,5 +1,14 @@
 import "./Placeholder.css"
 
-export default function Placeholder() {
-  return <div className="placeholder">PLACEHOLDER</div>
+interface PlaceholderProps {
+  bottomText?: string
+}
+
+export default function Placeholder({bottomText}: PlaceholderProps) {
+  return <div className="placeholder">
+    <div className="titleText">PLACEHOLDER</div>
+    {bottomText ?
+      <div className="bottomText">{bottomText}</div> : <></>
+    }
+  </div>
 }
