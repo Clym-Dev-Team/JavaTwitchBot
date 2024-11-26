5d3690a23ab48fc7e45399f83ec12f68b1738d30
orciument <orciument@gmail.com>
2024-11-26 18:18:27 +0100

    extract editor and leaderboard

---
+++ panel/src/components/watchtime/WatchtitmeLeaderboard.tsx
@@ -0,0 +1,44 @@
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "../../../@shadcn/components/ui/table.tsx";
+import Loader from "../../common/LoadingSpinner/Loader.tsx";
+import useData from "../../common/useData.ts";
+import {ScrollArea} from "../ui/scroll-area.tsx";
+
+interface LeaderboardDTO {
+    twitchUserName: string,
+    coins: number,
+    watchtimeSeconds: number,
+}
+
+export default function WatchtitmeLeaderboard() {
+    const {data, loading} = useData<LeaderboardDTO[]>("/watchtime/top", "WatchtimeLeaderboard", [ {
+        twitchUserName: "test",
+        coins: 1929,
+        watchtimeSeconds: 292907192393
+    }])
+
+    return <ScrollArea className="watchtimeLeaderboard">
+      <h1>Top 100 Leaderboard - Watchtime:</h1>
+      <Table>
+          <TableHeader>
+              <TableRow>
+                  <TableHead className="tw-w-10">#</TableHead>
+                  <TableHead>Username</TableHead>
+                  <TableHead className="tw-w-32">Coins</TableHead>
+                  <TableHead>Seconds</TableHead>
+                  <TableHead className="tw-w-32">Days</TableHead>
+              </TableRow>
+          </TableHeader>
+          <TableBody>
+              {!loading ? data.map((value, index) => (
+                <TableRow key={index}>
+                    <TableCell>{index}</TableCell>
+                    <TableCell>{value.twitchUserName}</TableCell>
+                    <TableCell>{value.coins}</TableCell>
+                    <TableCell>{value.watchtimeSeconds}</TableCell>
+                    <TableCell>{value.watchtimeSeconds / 86400}</TableCell>
+                </TableRow>
+              )) : <Loader/>}
+          </TableBody>
+      </Table>
+  </ScrollArea>
+}
